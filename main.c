b6510281ff31e6911e69001f537a30eef01f2177
Mustafa Yanikdorugu <yanikdorugumustafa@gmail.com>
2024-02-11 22:26:56 +0300

    adress ofsett was revized for api flexspi and word index was revized.

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
-#include "main.h"
-#include "stdio.h"
+#include<stdint.h>
 #define PAGESIZE 16
 #define WORDSIZE 64 
-#define FLASHBASEADDRESS NULL
-uint32_t flexspibellek[4096];
-uint32_t *ptrflexspibellek=flexspibellek;
-Flash_t bellek;
-      void flexspi_nor_flash_page_program(const int* BASEADDRESS,uint32_t address,const uint32_t *buffer)
+ typedef struct Flash_t
 {
-	
-    for(int i=0;i<64;i++)
-    {
-        *(ptrflexspibellek+address+4)=buffer[i];
-    
-    }
-	
-}
+    uint32_t BUFFER[16][64];
+}Flash_t;
 
 
 
+
+Flash_t bellek;
 
 void flash_func_write_word(uint32_t address, uint32_t word)
 {
@@ -30,7 +21,7 @@ void flash_func_write_word(uint32_t address, uint32_t word)
 
         if(wordIndex==63) /**one page receiving was completed*/
        {
-       flexspi_nor_flash_page_program(FLASHBASEADDRESS,address-0x100,&bellek.BUFFER[pageIndex][wordIndex]);
+       flexspi_nor_flash_page_program(FLASHBASEADDRESS,address+0xFC,&bellek.BUFFER[pageIndex][0]);
        /**
         * address-256 -> if one page receiving was completed, adress have 0x0000+(4*64) and we want to write start address its.
         * if start adress 0x100 page index = 1 and word index =0 and  buffer[1][0] its mean that we start to write another page.
@@ -46,20 +37,3 @@ void flash_func_write_word(uint32_t address, uint32_t word)
 
 }
 
-
-int main()
-{
-    uint32_t address=0x0000;
-    uint32_t uartcomeinword=0x0a0b0c0d;
-    for(int i=0;i<64*16;i++)
- {  
-    flash_func_write_word(address,uartcomeinword);
-    uartcomeinword++;
-    address+=4;
- 
- }
-
-
-
-
-}
